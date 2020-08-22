62cc85d58f2989627f4367f98e469318a34b0b60
kylest0kes <kylestokes1551@gmail.com>
2020-08-22 09:54:56 -0400

    added new header with links that can used to route to other pages

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -3,8 +3,14 @@ import './style.css'
 
 export default function Header() {
     return (
-        <header>
-            <h1>Ink Central</h1>
+        <header className="subnav-hero-section">
+            <h1 className="subnav-hero-headline">Ink Central</h1>
+            <ul className="subnav-hero-subnav">
+                <li><a href="https://zurb.com/responsive">User</a></li>
+                <li><a href="https://zurb.com/responsive">View All</a></li>
+                <li><a href="https://zurb.com/responsive">Ink For Grabs</a></li>
+                <li><a href="https://zurb.com/responsive">Looking For Ink</a></li>
+            </ul>
         </header>
     )
 }
