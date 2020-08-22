9623fd9b2177ed6314cae0f28906aabf5bcda10b
Bryce Speirs <baspeirs@gmail.com>
2020-08-22 12:57:10 -0400

    finished log in and sign up functionality and able to route user to home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import ViewAllPage from './pages/ViewAllPage'
-// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import SignupPage from './pages/SignupPage';
-// import LoginPage from './pages/LoginPage';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SignupPage from './pages/SignupPage';
+import LoginPage from './pages/LoginPage';
 import './App.css';
 import LookingForInk from './pages/LookingForInk';
 
 function App() {
   return (
-    // <Router>
-    //   <div className="App">
-    //     <Switch>
-    //       <Route exact path="/">
-    //         <SignupPage />
-    //       </Route>
-    //       <Route path="/login">
-    //         <LoginPage />
-    //       </Route>
-    //     </Switch>
-    //   </div>
-    // </Router>
-    //<ViewAllPage />
-    <LookingForInk />
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <SignupPage />
+          </Route>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          <Route path="/home">
+              <ViewAllPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    // <ViewAllPage />
+    // <LookingForInk />
   );
 }
 
--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import './style.css'
 
-export default function Login() {
+export default function Login(props) {
     return (
         <div className="translucent-form-overlay">
             <h3>Login</h3>
@@ -10,22 +10,24 @@ export default function Login() {
                 <label>Username
                     <input 
                         type="text" 
-                        name="username" 
-                        
+                        name="username"
+                        value={props.userState.username} 
+                        onChange={props.handleInputChange}
                     />
                 </label>
             </div>
             <div className="row columns">
                 <label>Password
                     <input 
-                        type="password" 
-                        name="password"
-                        
+                        type="password"
+                        name="password" 
+                        value={props.userState.password}
+                        onChange={props.handleInputChange}
                     /> 
                 </label>
             </div>
-            <button type="submit" className="primary button expanded search-button">
-            Sign Up
+            <button type="submit" name="login" className="primary button expanded search-button" onClick={props.login}>
+            Log in
             </button>
             <Link style={{color:"white", textDecoration:"underline"}} to="/">Sign up here.</Link>    
         </div>
