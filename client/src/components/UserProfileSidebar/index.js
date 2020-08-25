7a194c15737ef9ed5574348f533eb935eacabdc7
kylest0kes <kylestokes1551@gmail.com>
2020-08-24 22:06:51 -0400

    began user profile template, created component for user profil posts

---
+++ client/src/components/UserProfileSidebar/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function UserProfileSidebar() {
+    return (
+        <div style={{top: "200px", bottom: "200px"}} className="off-canvas position-left reveal-for-large" id="my-info" data-off-canvas>
+            <div className="grid-y grid-padding-x" style={{height: "100%"}}>
+                <br/>
+                <div className="cell shrink">
+                    <img alt="img" className="thumbnail" src="https://placehold.it/350x350"/>
+                </div>
+                <div style={{textAlign: "center"}} className="cell auto">
+                    <h4>Username</h4>
+                    <hr/>
+                    <h6>User Bio</h6>
+                </div>
+            </div>
+        </div>
+    )
+}
