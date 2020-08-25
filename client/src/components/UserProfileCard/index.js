b5b749e6e32972c10be0b9abb691df6eee2a546b
kylest0kes <kylestokes1551@gmail.com>
2020-08-24 22:23:59 -0400

    finished components for the user profile page and completed the layout for the page

--- client/src/components/UserProfileCard/index.js
+++ client/src/components/UserProfileCard/index.js
@@ -6,11 +6,12 @@ import './style.css'
 export default function UserProfileCard() {
     return (
         <div className="image-hover-wrapper">
-            <span className="image-hover-wrapper-banner">Old Junky Bikes</span>
-            <a href="mailto:artistemail@email.com"><img alt="img" src="https://via.placeholder.com/300"/>
             {/* this will be a link to the email artists about post */}
+            <span className="image-hover-wrapper-banner">Post Title</span>
+            <a href="mailto:artistemail@email.com"><img alt="img" src="https://via.placeholder.com/300"/>
             <span className="image-hover-wrapper-reveal">
                 <p>Message Artist<br/><i class="far fa-paper-plane"></i></p>
+                <p>"Post Description"</p>
             </span>
             </a>
         </div>
