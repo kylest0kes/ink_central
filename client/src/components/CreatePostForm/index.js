6a8fbecded00847e0113510cbdc113347fb03f97
kylest0kes <kylestokes1551@gmail.com>
2020-08-27 19:38:41 -0400

    linked up filestack API to pop up as modal and have access to images for upload

--- client/src/components/CreatePostForm/index.js
+++ client/src/components/CreatePostForm/index.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
-
+import ReactFilestack from 'filestack-react';
 
 import './style.css'
 
 export class CreateFormPost extends Component {
+
+    onSuccess = (result) => {
+        console.log(result)
+    }
+    
+    onError = (error) => {
+        console.error('error', error);
+    }
+
     render() {
         return (
             <form>
@@ -15,10 +24,14 @@ export class CreateFormPost extends Component {
                     <label for="exampleFormControlTextarea1">Description</label>
                     <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                 </div>
-                <div className="form-group">
-                    <label for="exampleFormControlInput1">Upload Image</label>
-                    <button><i className="fas fa-file-upload"></i></button>
-                </div>
+                <ReactFilestack
+                    apikey={"Aho8KdLp5TCKolnTXxw3Tz"}
+                    buttonText="Upload Photo"
+                    buttonClass="ui medium button gray"
+                    //options={basicOptions}
+                    onSuccess={this.onSuccess}
+                    onError={this.onError}
+                />
                 <div className="form-group">
                     <input style={{backgroundColor: "#00adcc", borderColor: "#00adcc"}} className="btn btn-primary" type="submit" value="Submit"/>
                 </div>
