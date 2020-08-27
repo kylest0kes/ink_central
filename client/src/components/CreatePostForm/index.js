a3e40920c2b8830d407bf466696989426e8b0d24
kylest0kes <kylestokes1551@gmail.com>
2020-08-27 19:11:20 -0400

    converted createformpost to class component

--- client/src/components/CreatePostForm/index.js
+++ client/src/components/CreatePostForm/index.js
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 
 import './style.css'
 
-export default function CreatePostForm() {
-    return (
-        <form>
-            <div className="form-group">
-                <label for="exampleFormControlInput1">Title</label>
-                <input type="email" className="form-control" id="exampleFormControlInput1"/>
-            </div>
-            <div className="form-group">
-                <label for="exampleFormControlTextarea1">Description</label>
-                <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-            </div>
-            <div className="form-group">
-                <label for="exampleFormControlInput1">Upload Image</label>
-                <button><i className="fas fa-file-upload"></i></button>
-            </div>
-            <div className="form-group">
-                <input style={{backgroundColor: "#00adcc", borderColor: "#00adcc"}} className="btn btn-primary" type="submit" value="Submit"/>
-            </div>
-        </form>
-    )
+export class CreateFormPost extends Component {
+    render() {
+        return (
+            <form>
+                <div className="form-group">
+                    <label for="exampleFormControlInput1">Title</label>
+                    <input type="email" className="form-control" id="exampleFormControlInput1"/>
+                </div>
+                <div className="form-group">
+                    <label for="exampleFormControlTextarea1">Description</label>
+                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+                <div className="form-group">
+                    <label for="exampleFormControlInput1">Upload Image</label>
+                    <button><i className="fas fa-file-upload"></i></button>
+                </div>
+                <div className="form-group">
+                    <input style={{backgroundColor: "#00adcc", borderColor: "#00adcc"}} className="btn btn-primary" type="submit" value="Submit"/>
+                </div>
+            </form>
+        )
+    }
 }
+
+export default CreateFormPost
+
