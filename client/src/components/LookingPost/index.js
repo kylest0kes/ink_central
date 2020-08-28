b016fed53764907f6cc842c0ca5a9e48149ce718
Bryce Speirs <baspeirs@gmail.com>
2020-08-28 18:55:45 -0400

    able to filter and display images with a Looking type

--- client/src/components/LookingPost/index.js
+++ client/src/components/LookingPost/index.js
@@ -3,15 +3,15 @@ import React from 'react';
 
 import './style.css'
 
-export default function LookingPost() {
+export default function LookingPost(props) {
     return (
         <article className="grid-container">
             <div className="grid-x align-center">
                 <div className="cell medium-8">
                     <div className="blog-post">
-                        <h3>Post Title</h3>
-                        <img className="thumbnail" alt="postImage" src="https://placehold.it/250x250"/>
-                        <p>Post Description</p>
+                        <h3>{props.title}</h3>
+                        <img className="thumbnail" alt="postImage" src={props.image}/>
+                        <p>{props.description}</p>
                         <div className="callout">
                             <ul className="menu simple">
                                 <li><a href="https://google.com" target="blank">UserProfileLink</a></li>
--- client/src/pages/LookingForInkPage.js
+++ client/src/pages/LookingForInkPage.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from '../components/Header';
 import LookingPost from '../components/LookingPost';
 import PostModal from '../components/PostModal';
+import API from "../utils/API";
 
 export default function LookingForInk(props) {
+    const [posts, setPosts] = useState({
+        postArr: []
+    });
+
+    useEffect(() => {
+        API.getPosts()
+        .then(res => {
+            console.log(res.data);
+            res.data.forEach(post => {
+                if(post.type === "Looking") {
+                    setPosts({postArr: res.data});
+                }
+            }); 
+        });
+    }, []);
+
     return (
         <div>
             <Header logout={props.logout}/>
-            <LookingPost />
-            <LookingPost />
-            <LookingPost />
-            <LookingPost />
-            <LookingPost />
+            {posts.postArr.map(post => (
+                <LookingPost 
+                    id={post.id}
+                    title={post.title}
+                    description={post.description}
+                    image={post.image}
+                    type={post.type}
+                    author={post.author}
+                />))}
             <PostModal 
                 authState={props.authState}
             />
