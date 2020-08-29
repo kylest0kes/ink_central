6c284815b573ed910ad4c9d2fadc616d0aceca42
kylest0kes <kylestokes1551@gmail.com>
2020-08-29 14:02:23 -0400

    fixed overflow text issue on available page

--- client/src/components/AvailablePost/index.js
+++ client/src/components/AvailablePost/index.js
@@ -7,12 +7,12 @@ export default function AvailablePost(props) {
     return (
         <div className="wide-article-link">
             <div className="row">
-                <div style={{width: "65%", height: "235px", overflow: "auto"}} className="text-area small-12 medium-9 columns">
-                    <h4 className="article-title">
+                <div style={{width: "65%"}} className="text-area small-12 medium-9 columns">
+                    <h4 style={{marginLeft: "8px"}}  className="article-title">
                         {props.title}
                     </h4>
                     <p className="article-author"><em>Artist: <a href="#">{props.user}</a></em></p>
-                    <p className="article-elipsis">{props.description}</p>
+                    <p style={{height: "170px", overflow: "auto"}} className="article-elipsis">{props.description}</p>
                 </div>
                 <div className="small-12 medium-3 columns flex-container availableImg">
                     <img src={props.image}/>
