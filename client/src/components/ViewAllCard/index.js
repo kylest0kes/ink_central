3cb7bbc16cf86c427d05b92dda732a09c21d5fa3
kylest0kes <kylestokes1551@gmail.com>
2020-08-29 10:20:32 -0400

    got view all page with a container that shirks with page shirking

--- client/src/components/ViewAllCard/index.js
+++ client/src/components/ViewAllCard/index.js
@@ -5,7 +5,7 @@ import './style.css';
 
 export default function ViewAllCard(props) {
     return (
-        <div className="image-hover-wrapper">
+        <div className="cell image-hover-wrapper">
             <span className="image-hover-wrapper-banner">{props.title}</span>
             {/* this will be a link to the artists user profile */}
             <a href="https://github.com"><img alt="img" src={props.image}/>
