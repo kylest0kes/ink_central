85d244219cb90603fa751cc60d63c37c20dc98e9
kylest0kes <kylestokes1551@gmail.com>
2020-08-29 11:41:14 -0400

    fixed layout of the userhome postcard

--- client/src/components/UserPostCard/index.js
+++ client/src/components/UserPostCard/index.js
@@ -4,9 +4,9 @@ import './style.css'
 
 export default function UserPostCard() {
     return (
-        <article style={{ marginRight: "15px", marginLeft: "15px", width: "100%"}} className="grid-container">
+        <article className="grid-container">
             <div className="grid-x grid-margin-x small-up-2 medium-up-3 large-up-4">
-                <div style={{width: "100%"}} className="travel-feature-card">
+                <div style={{width: "90%"}} className="travel-feature-card">
                     <div className="travel-feature-card-header" style={{height: "40px"}}>
                         <div className="row">
                             <div className="medium-12 columns">
@@ -20,12 +20,12 @@ export default function UserPostCard() {
                     <div className="travel-feature-card-details">
                         <div className="row">
                             <div className="small-12 medium-9 columns travel-feature-card-content">
-                                <div className="row">
+                                <div style={{marginLeft: "10px"}} className="row">
                                     <div className="small-4 medium-2 columns">
                                         <img className="travel-feature-card-image" src="https://placehold.it/250x250" alt=""/>
                                     </div>
                                     <div className="small-8 medium-10 columns">
-                                        <h6 className="travel-feature-card-title">Title of the Users Post</h6>
+                                        <h6 style={{marginLeft: "10px"}} className="travel-feature-card-title">Title of the Users Post</h6>
                                         <p>Description from the Users Post</p>
                                     </div>
                                 </div> 
