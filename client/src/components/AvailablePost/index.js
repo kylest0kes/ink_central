822bd8eb3dda287b59ad6cfbae7e07cfeaf869ab
kylest0kes <kylestokes1551@gmail.com>
2020-08-29 11:58:37 -0400

    fixed the availableink page layout

--- client/src/components/AvailablePost/index.js
+++ client/src/components/AvailablePost/index.js
@@ -7,14 +7,14 @@ export default function AvailablePost(props) {
     return (
         <div className="wide-article-link">
             <div className="row">
-                <div className="text-area small-12 medium-9 columns">
+                <div style={{width: "65%", height: "235px", overflow: "auto"}} className="text-area small-12 medium-9 columns">
                     <h4 className="article-title">
                         {props.title}
                     </h4>
                     <p className="article-author"><em>Artist: {props.user}<a href="#">Artist Username as Link</a></em></p>
                     <p className="article-elipsis">{props.description}</p>
                 </div>
-                <div className="small-12 medium-3 columns flex-container">
+                <div className="small-12 medium-3 columns flex-container availableImg">
                     <img src={props.image}/>
                 </div>
             </div>
